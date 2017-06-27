a8c3927c309c90fb31d28a42e42b03ca234d0403
squeemishly <squeemishly@gmail.com>
2017-06-26 21:00:27 -0600

    item api can be accessed and can find the names and descriptions in the index

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe "Items requests" do
 
     get '/api/v1/items'
 
+    items = JSON.parse(response.body)
+    item = items.first
+
     expect(response).to be_success
+    expect(items.count).to be 3
+    expect(item).to have_key "name"
+    expect(item).to have_key "description"
+    expect(item["name"]).to be_a String
+    expect(item["description"]).to be_a String
   end
 end
