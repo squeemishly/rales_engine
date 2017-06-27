0dac02cd4cfb1610fc55dbf1bae2a2bda21cbbe4
squeemishly <squeemishly@gmail.com>
2017-06-27 11:25:10 -0600

    changed unit_price on items table to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627171713) do
+ActiveRecord::Schema.define(version: 20170627172129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170627171713) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "unit_price"
+    t.string "unit_price"
     t.integer "merchant_id"
   end
 
