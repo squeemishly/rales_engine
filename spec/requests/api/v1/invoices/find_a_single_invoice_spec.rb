123898339369dbc6eb48a178ed19ba290d35bb3c
squeemishly <squeemishly@gmail.com>
2017-06-27 16:45:27 -0600

    find all invoices that match a given condition

--- spec/requests/api/v1/invoices/find_a_single_invoice_spec.rb
+++ spec/requests/api/v1/invoices/find_a_single_invoice_spec.rb
@@ -34,4 +34,59 @@ RSpec.describe "find an invoice" do
       expect(invoice_parsed["merchant_id"]).to eq invoice.merchant_id
     end
   end
+
+  context "finds all invoices" do
+    it "by the status" do
+      invoice1 = create(:invoice)
+      invoice2 = create(:invoice)
+      invoice3 = create(:invoice, status: "horrid")
+      invoice4 = create(:invoice, status: "blue")
+
+      get '/api/v1/invoices/find_all?status=refreshing'
+
+      request = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(response.body).to include(invoice1.status)
+      expect(response.body).to include(invoice2.status)
+      expect(response.body).to_not include(invoice3.status)
+      expect(response.body).to_not include(invoice4.status)
+    end
+
+    it "by the customer_id" do
+      customer = create(:customer)
+      invoice1 = create(:invoice)
+      invoice2 = create(:invoice)
+      invoice3 = create(:invoice, customer_id: customer.id)
+      invoice4 = create(:invoice, customer_id: customer.id)
+
+      get "/api/v1/invoices/find_all?customer_id=#{customer.id}"
+
+      request = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(response.body).to include(invoice3.customer_id.to_s)
+      expect(response.body).to include(invoice4.customer_id.to_s)
+      expect(response.body).to_not include(invoice1.customer_id.to_s)
+      expect(response.body).to_not include(invoice2.customer_id.to_s)
+    end
+
+    it "by the merchant_id" do
+      merchant = create(:merchant)
+      invoice1 = create(:invoice)
+      invoice2 = create(:invoice)
+      invoice3 = create(:invoice, merchant_id: merchant.id)
+      invoice4 = create(:invoice, merchant_id: merchant.id)
+
+      get "/api/v1/invoices/find_all?merchant_id=#{merchant.id}"
+
+      request = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(response.body).to include(invoice3.merchant_id.to_s)
+      expect(response.body).to include(invoice4.merchant_id.to_s)
+      expect(response.body).to_not include(invoice1.merchant_id.to_s)
+      expect(response.body).to_not include(invoice2.merchant_id.to_s)
+    end
+  end
 end
