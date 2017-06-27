c8b66023697bc102221817adb5656456bd1aa28b
Beth Knight <bethknight1234@gmail.com>
2017-06-27 17:00:15 -0600

    find and find all for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
 
   namespace :api do
      namespace :v1 do
+
+       namespace :merchants do
+        get "/find_all", to: "find#index"
+        get "/find", to: "find#show"
+        get "/random", to: "random#show"
+       end
+
        resources :merchants, only: [:index, :show]
        resources :customers, only: [:index, :show]
        resources :invoices, only: [:index, :show]
