3e20bf2ab1988712514d2877eab14257fe799818
Beth Knight <bethknight1234@gmail.com>
2017-06-28 06:49:54 -0600

    finds transaction controller

--- app/controllers/api/v1/transactions/find_controller.rb
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Transactions::FindController < ApplicationController
 
   def index
-    render json: Transactions.where(transaction_params)
+    render json: Transaction.where(transaction_params)
   end
 
   def show
-    render json: Transactions.find_by(transaction_params)
+    render json: Transaction.find_by(transaction_params)
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,11 @@ Rails.application.routes.draw do
 
        resources :invoices, only: [:index, :show]
 
+       namespace :transactions do
+         get "/find", to: "find#show"
+         get "/find_all", to: "find#index"
+       end
+
        resources :transactions, only: [:index, :show] do
          get '/invoice', to: 'transactions/transactions_invoice#show'
        end
