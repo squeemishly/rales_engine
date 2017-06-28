ce2809fd014e03df9086e4a356b7e38f515c70e9
Beth Knight <bethknight1234@gmail.com>
2017-06-28 17:11:05 -0600

    pushing again dammit

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -7,4 +7,5 @@ FactoryGirl.define do
         1
     end
   end
+  
 end
--- spec/requests/api/v1/invoice_items/invoice_items_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_items_request_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe "Invoice Items API" do
-  it "returns an invoice associated with an invoice item" do
+  it "returns an invoice associate with an invoice item" do
 
     item1 = create(:item)
     invoice_item1 = create(:invoice_item, item: item1)
 
     get "/api/v1/invoice_items/#{invoice_item1.id}/item"
+
     expect(response).to be_success
 
     item = JSON.parse(response.body)
