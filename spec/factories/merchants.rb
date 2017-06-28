1feed46390c348b316cdac33844c389858b242a3
squeemishly <squeemishly@gmail.com>
2017-06-27 19:35:41 -0600

    can now see a list of items associated with each merchant

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
   factory :merchant do
     name "MyString"
+
+    factory :merchants_with_items do
+      transient do
+        items_count 3
+      end
+
+      after(:create) do |merchant, evaluator|
+        create_list(:item, evaluator.items_count, merchant: merchant)
+      end
+    end
   end
 end
