355b88aef34b063286b60c1391a42de7e54371c3
squeemishly <squeemishly@gmail.com>
2017-06-28 14:15:20 -0600

    resolved merge conflict

--- app/controllers/api/v1/customers/transaction_controller.rb
+++ app/controllers/api/v1/customers/transaction_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Customers::TransactionController < ApplicationController
   def index
-    binding.pry
-    render json: Transaction.where("invoice_id: Customer.find(params[:id]).invoices")
+    render json: Customer.find(params[:id]).transactions
   end
 end
--- spec/requests/api/v1/customers/transactions_by_customer_spec.rb
+++ spec/requests/api/v1/customers/transactions_by_customer_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "a list of transactions" do
     customer1 = create(:customer)
     customer2 = create(:customer)
 
-    invoice1 = create(:invoice, customer_id: customer1.id)
-    invoice2 = create(:invoice, customer_id: customer1.id)
-    invoice3 = create(:invoice, customer_id: customer2.id)
+    invoice1 = create(:invoice, customer_id: customer1.id, id: 100000000)
+    invoice2 = create(:invoice, customer_id: customer1.id, id: 200000000)
+    invoice3 = create(:invoice, customer_id: customer2.id, id: 300000000)
 
     transaction1 = create(:transaction, invoice_id: invoice1.id)
     transaction2 = create(:transaction, invoice_id: invoice2.id)
@@ -17,5 +17,9 @@ RSpec.describe "a list of transactions" do
     transactions = JSON.parse(response.body)
 
     expect(response).to be_success
+    expect(transactions.first).to have_key "credit_card_number"
+    expect(response.body).to include invoice1.id.to_s
+    expect(response.body).to include invoice2.id.to_s
+    expect(response.body).to_not include invoice3.id.to_s
   end
 end
