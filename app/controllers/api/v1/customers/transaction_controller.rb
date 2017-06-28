412ab22e30ee90444ec356a75f8acccba93fc754
squeemishly <squeemishly@gmail.com>
2017-06-28 08:40:28 -0600

    test expanded, notes made on transaction_controller

--- app/controllers/api/v1/customers/transaction_controller.rb
+++ app/controllers/api/v1/customers/transaction_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Customers::TransactionController < ApplicationController
   def index
     binding.pry
+    render json: Transaction.where("invoice_id: Customer.find(params[:id]).invoices")
   end
 end
