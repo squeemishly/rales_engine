c93f125709218b3ab0428f129d090b9b8002890f
squeemishly <squeemishly@gmail.com>
2017-06-28 09:00:45 -0600

    find and find all created for items

---
+++ app/controllers/api/v1/items/find_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::Items::FindController < ApplicationController
+  def show
+    render json: Item.find_by(item_params)
+  end
+
+  def index
+    render json: Item.where(item_params)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
