f793c02ed33f2ca8a5e26af46228501b78d8c8c2
squeemishly <squeemishly@gmail.com>
2017-06-28 19:11:17 -0600

    Item calculates which item has earned the most revenue

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
        namespace :items do
          get '/find', to: 'find#show'
          get '/find_all', to: 'find#index'
+         get '/most_revenue', to: "most_revenue#index"
        end
 
        resources :items, only: [:index, :show] do
