689319b7e6de23d6805ac945973e2705bcdf33f1
squeemishly <squeemishly@gmail.com>
2017-06-29 11:48:37 -0600

    test written

--- spec/requests/api/v1/merchants/favorite_customer_spec.rb
+++ spec/requests/api/v1/merchants/favorite_customer_spec.rb
@@ -9,11 +9,24 @@ RSpec.describe "a merchants favorite customer can be found" do
     customer3 = create(:customer)
 
     invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
-    invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
-    invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
-    invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
-    invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
-    invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
-    invoice1 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+    invoice2 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+    invoice3 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+    invoice4 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+    invoice5 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+    invoice6 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+    invoice7 = create(:invoice, customer_id: customer1, merchant_id: merchant)
+
+    create(:transaction, invoice_id: invoice1)
+    create(:transaction, invoice_id: invoice2)
+    create(:transaction, invoice_id: invoice3)
+    create(:transaction, invoice_id: invoice3, result: "failed")
+    create(:transaction, invoice_id: invoice4)
+    create(:transaction, invoice_id: invoice5)
+    create(:transaction, invoice_id: invoice6)
+    create(:transaction, invoice_id: invoice7)
+
+    get "/api/v1/merchants/#{merchant.id}/favorite_customer"
+
+    expect(response).to be_success
   end
 end
